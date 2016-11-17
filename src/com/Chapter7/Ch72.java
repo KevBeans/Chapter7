640a80fafd719da23e8ccc534cec4124092899a8
KevBeans <kevooi.of.estland@gmail.com>
2016-11-17 11:16:05 +0200

    completed

--- src/com/Chapter7/Ch72.java
+++ src/com/Chapter7/Ch72.java
@@ -2,11 +2,18 @@ package com.Chapter7;
 
 public class Ch72 {
 
-    public static double squareRoot(double x) {
-        return 1;
+    public static double squareRoot(double a) {
+        double x0 = 0;
+        double x1 = a / 2;
+        while (Math.abs(x1 - x0) > 0.0001) {
+            x0 = x1;
+            x1 = (x0 + a / x0) / 2;
+        }
+        return x1;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(squareRoot(49));
+        System.out.println(squareRoot(100));
     }
 }
