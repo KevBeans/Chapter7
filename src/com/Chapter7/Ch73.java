758c5800791f47a05cee12cccacb50e18f318ac3
KevBeans <kevooi.of.estland@gmail.com>
2016-11-18 11:03:58 +0200

    update

--- src/com/Chapter7/Ch73.java
+++ src/com/Chapter7/Ch73.java
@@ -19,7 +19,6 @@ public class Ch73 {
     public static void main(String[] args) {
         float x = 2.0f;
         int n = 4;
-        System.out.println("Test");
         System.out.println(x + " raised to the power of " + n + " equals " + power(x, n));
     }
 }
