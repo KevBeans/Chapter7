42158599199d17228925e8d2cc8a6da4130276aa
KevBeans <kevooi.of.estland@gmail.com>
2016-11-18 11:42:56 +0200

    update

--- src/com/Chapter7/Ch74.java
+++ src/com/Chapter7/Ch74.java
@@ -2,7 +2,21 @@ package com.Chapter7;
 
 public class Ch74 {
 
+    public static int factorial(int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+        int recurse = factorial(n - 1);
+        int result = n * recurse;
+        System.out.println("The factorial of " + n + " is " + result);
+        return result;
+    }
+
     public static void main(String[] args) {
 
+        factorial(10);
     }
 }
+
+//CONVERT FROM RECURSIVE TO ITERATIVE
